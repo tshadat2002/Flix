e5ea3ad85f9a85aaab9beb280d435f94ee2345ce
tshadat2002 <tshadat2968@bths.edu>
2021-07-16 00:50:33 -0400

    FLix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Taohid Shadat on 7/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
